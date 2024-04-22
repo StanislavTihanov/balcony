e6987637d599cc2fc7acf46270a5c9e68ead380a
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-04-22 18:18:03 +0300

    Добавил вопрос ответ

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 "use strict"
 
-//------------------------------------------------------------------------preloader
-//document.body.onload = () => {
-//  setTimeout(() => {
-//    let preloader = document.getElementById('preloader');
-//    if (!preloader.classList.contains('done')) {
-//      preloader.classList.add('done');
-//    }
-//  }, 1000);
-//}
-//------------------------------------------------------------------------preloader
+jQuery(document).ready(function () {
+    
+  $(".phone").mask("+7 (999) 999-99-99"); 
+  
+  jQuery('.send-form').click( function() {
+    var form = jQuery(this).closest('form');
+    
+    if ( form.valid() ) {
+      form.css('opacity','.5');
+      var actUrl = form.attr('action');
+
+      jQuery.ajax({
+        url: actUrl,
+        type: 'post',
+        dataType: 'html',
+        data: form.serialize(),
+        success: function(data) {
+          form.html(data);
+          form.css('opacity','1');
+                  //form.find('.status').html('форма отправлена успешно');
+                  //$('#myModal').modal('show') // для бутстрапа
+        },
+        error:	 function() {
+            form.find('.status').html('серверная ошибка');
+        }
+      });
+    }
+  });
+
+
+});
 
 const openHeaderMenu = document.querySelector('.header__services-button');
 const headerMenuList = document.querySelector('.header__list-wrapper');
@@ -378,13 +399,21 @@ const worksSlider = new Swiper('.works__slider', {
 const designSlider = new Swiper('.design__slider', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 20,
   speed: 1000,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    880: {
+      slidesPerView: 2,
+    },
+  }
  });
 
 
@@ -401,7 +430,7 @@ const designSlider = new Swiper('.design__slider', {
   },
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
+    clickable: false,
   },
   breakpoints: {
     320: {
@@ -417,6 +446,19 @@ const designSlider = new Swiper('.design__slider', {
  });
 
 
+ const projectsSlider = new Swiper('.projects__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  speed: 1000,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
 //------------------------------------------------------------------------Слайдер
 
 
@@ -429,7 +471,7 @@ const designSlider = new Swiper('.design__slider', {
   },
   animation: {
     enable: false
-}
+  }
  });
 
 //------------------------------------------------------------------------Слайдер
@@ -445,104 +487,88 @@ $(function(){
 
 
 //------------------------------------------------------------------------popup
-//const popupLinks = document.querySelectorAll('.popup-link');
-//const body = document.querySelector('body');
-//const lockPadding = document.querySelectorAll(".lock-padding");
-//
-//
-//let unlock = true;
-//
-//const timeout = 800;
-//
-//if (popupLinks.length > 0) {
-//  for (let index = 0; index < popupLinks.length; index++) {
-//    const popupLink = popupLinks[index];
-//    popupLink.addEventListener("click", function (e) {
-//      const popupName = popupLink.getAttribute('href').replace('#', '');
-//      const currentPopup = document.getElementById(popupName);
-//      popupOpen(currentPopup);
-//      e.preventDefault();
-//    });
-//  }
-//}
-//
-//const popupCloseIcon = document.querySelectorAll('.close-popup');
-//if (popupCloseIcon.length > 0) {
-//  for (let index = 0; index < popupCloseIcon.length; index++) {
-//    const el = popupCloseIcon[index];
-//    el.addEventListener('click', function (e) {
-//      popupClose(el.closest('.popup'));
-//      e.preventDefault();
-//    })
-//  }
-//}
-//
-//function popupOpen(currentPopup) {
-//  if (currentPopup && unlock) {
-//    const popupActive = document.querySelector('.popup.open');
-//    if (popupActive) {
-//      popupClose(popupActive, false);
-//    } else {
-//      bodyLock();
-//    }
-//    currentPopup.classList.add('open');
-//    currentPopup.addEventListener("click", function (e) {
-//      if (!e.target.closest('.popup__content')) {
-//        popupClose(e.target.closest('.popup'));
-//      }
-//    });
-//  }
-//}
-//
-//function popupClose(popupActive, doUnlock = true) {
-//  if (unlock) {
-//    popupActive.classList.remove('open');
-//    if (doUnlock) {
-//      bodyUnlock();
-//    }
-//  }
-//}
-//
-//function bodyLock() {
-//  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-//  if (lockPadding.length > 0) {
-//    for (let index = 0; index < lockPadding.length; index++) {
-//      const el = lockPadding[index];
-//      el.style.paddingRight = lockPaddingValue;
-//    }
-//  }
-//  body.style.paddingRight = lockPaddingValue;
-//  body.classList.add('lock');
-//
-//  unlock = false;
-//  setTimeout(function () {
-//    unlock = true;
-//  }, timeout);
-//}
-//
-//function bodyUnlock () {
-//  setTimeout(function () {
-//    if(lockPadding.length > 0) {
-//      for (let index = 0; index < lockPadding.length; index++) {
-//        const el = lockPadding[index];
-//        el.style.paddingRight = '0px';
-//      }
-//  }
-//    body.style.paddingRight = '0px';
-//    body.classList.remove('lock');
-//  }, timeout);
-//  unlock = false;
-//  setTimeout(function () {
-//    unlock = true;
-//  }, timeout);
-//}
-//
-//document.addEventListener('keydown', function (e) {
-//  if (e.which === 27) {
-//    const popupActive = document.querySelector('.popup.open');
-//    popupClose(popupActive);
-//  }
-//});
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+const timeout = 800;
+
+// Use event delegation for popup links
+document.addEventListener('click', function(e) {
+ if (e.target.matches('.popup-link')) {
+    const popupName = e.target.getAttribute('href').replace('#', '');
+    const currentPopup = document.getElementById(popupName);
+    popupOpen(currentPopup);
+    e.preventDefault();
+ }
+});
+
+// Use event delegation for popup close icons
+document.addEventListener('click', function(e) {
+ if (e.target.matches('.close-popup')) {
+    popupClose(e.target.closest('.popup'));
+    e.preventDefault();
+ }
+});
+
+function popupOpen(currentPopup) {
+ if (currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener("click", function(e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+ }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+ if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+      bodyUnlock();
+    }
+ }
+}
+
+function bodyLock() {
+ const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+ lockPadding.forEach(el => el.style.paddingRight = lockPaddingValue);
+ body.style.paddingRight = lockPaddingValue;
+ body.classList.add('lock');
+
+ unlock = false;
+ setTimeout(function() {
+    unlock = true;
+ }, timeout);
+}
+
+function bodyUnlock() {
+ setTimeout(function() {
+    lockPadding.forEach(el => el.style.paddingRight = '0px');
+    body.style.paddingRight = '0px';
+    body.classList.remove('lock');
+ }, timeout);
+ unlock = false;
+ setTimeout(function() {
+    unlock = true;
+ }, timeout);
+}
+
+document.addEventListener('keydown', function(e) {
+ if (e.which === 27) {
+    const popupActive = document.querySelector('.popup.open');
+    popupClose(popupActive);
+ }
+});
+
 
 //------------------------------------------------------------------------popup
 
